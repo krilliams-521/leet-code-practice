1cd0aeccc587254f1c5a957c8ef39d4a88206efa
krilliams-521 <25971011+krilliams-521@users.noreply.github.com>
2026-01-31 15:09:47 -0600

    problem 16 updates

--- 16-climbing-stairs/climbingStairs.js
+++ 16-climbing-stairs/climbingStairs.js
@@ -4,14 +4,14 @@
  */
 var climbStairs = function(n) {
   if (n === 1) return 1;
-  let first = 1;
-  let second = 2;
+  let firstStep = 1;
+  let secondStep = 2;
   for (let i = 3; i <= n; i++) {
-    let third = first + second;
-    first = second;
-    second = third;
+    let thirdStep = firstStep + secondStep;
+    firstStep = secondStep;
+    secondStep = thirdStep;
   }
-  return second;
+  return secondStep;
 };
 
 console.log(climbStairs(1)); // Output: 1

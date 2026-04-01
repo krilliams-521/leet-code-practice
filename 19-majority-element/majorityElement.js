295b2295b7fc865d0a6e512b3a973627c70c5f49
krilliams-521 <25971011+krilliams-521@users.noreply.github.com>
2026-03-31 20:53:54 -0500

    majority element

---
+++ 19-majority-element/majorityElement.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+
+var majorityElement = function(nums) {
+  let count = 0;
+  let candidate = null;
+  for (let num of nums) {
+    if (count === 0) {
+      candidate = num;
+    }
+    count += (num === candidate) ? 1 : -1;
+  }
+  return candidate;
+};
+
+console.log(majorityElement([3, 2, 3])); // Output: 3
+console.log(majorityElement([2, 2, 1, 1, 1, 2, 2])); // Output: 2

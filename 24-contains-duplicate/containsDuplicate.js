955d2a95fbaa4d299e9e1752aff6946798012bf3
krilliams-521 <25971011+krilliams-521@users.noreply.github.com>
2026-06-11 21:31:11 -0500

    contains duplicate

---
+++ 24-contains-duplicate/containsDuplicate.js
@@ -0,0 +1,23 @@
+/**
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+var containsDuplicate = function (nums) {
+
+  const seen = new Set();
+
+  for (const num of nums) {
+    if (seen.has(num)) {
+      return true;
+    }
+
+    seen.add(num);
+  }
+
+  return false;
+
+};
+
+console.log(containsDuplicate([1, 2, 3, 1])) // true
+console.log(containsDuplicate([1, 2, 3, 4])) // false
+console.log(containsDuplicate([1, 1, 1, 3, 3, 4, 3, 2, 4, 2])) // true
